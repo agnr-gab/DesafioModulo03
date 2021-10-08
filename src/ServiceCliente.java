15805d4e77e9607f847b881b63f8cfbbe530eb4a
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 13:06:27 -0300

    Criar cadastrarCliente

--- src/ServiceCliente.java
+++ src/ServiceCliente.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ServiceCliente {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceCliente {
+    //cadastrar clientes
+    //verificar clientes cadastrados
+    //listar clientes cadastrados
+    private static List<Cliente> listaClientes = new ArrayList<>();
+
+    public static Cliente cadastrarClientes(String nome, String cpf, String email) {
+        Cliente cliente = new Cliente(nome, cpf, email);
+        listaClientes.add(cliente);
+        return cliente;
+    }
 }
