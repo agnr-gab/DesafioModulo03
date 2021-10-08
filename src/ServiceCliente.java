16acff3fb115bcd1cf44074a8f01537f0809a540
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 15:35:25 -0300

    Criar metodo de validação cadastro cliente

--- src/ServiceCliente.java
+++ src/ServiceCliente.java
@@ -12,4 +12,28 @@ public class ServiceCliente {
         listaClientes.add(cliente);
         return cliente;
     }
+
+    public static void verificarClienteCadastro(String email, String cpf) throws Exception {
+        for (Cliente referenciaCliente : listaClientes) {
+            verificarEmailCliente(referenciaCliente.getEmail(), email);
+            verificarCpfCliente(referenciaCliente.getCpf(), cpf);
+        }
+    }
+
+    private static void verificarEmailCliente(String referencia, String email) throws Exception {
+        if (referencia.equalsIgnoreCase(email))
+            throw new Exception("Esse email já foi cadastrado! Tente novamente!");
+    }
+
+    private static void verificarCpfCliente(String referencia, String cpf) throws Exception {
+        if (referencia.equalsIgnoreCase(cpf))
+            throw new Exception("Esse CPF já foi cadastrado! Tente novamente!");
+    }
+
+    public static void verificarCpfCadastrado(String cpf) throws Exception {
+        for (Cliente referenciaCliente : listaClientes) {
+            if (!referenciaCliente.getCpf().equalsIgnoreCase(cpf))
+                throw new Exception("Esse CPF não está cadastrado! Tente novamente!");
+        }
+    }
 }
