9a0e19a14a3184dc430fba758731213433983c52
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 20:43:22 -0300

    Listar clientes cadastrados

--- src/ServiceCliente.java
+++ src/ServiceCliente.java
@@ -7,24 +7,6 @@ public class ServiceCliente {
     //listar clientes cadastrados
     private static List<Cliente> listaClientes = new ArrayList<>();
 
-    public static Cliente cadastrarCliente(String nome, String cpf, String email) throws Exception {
-        Cliente cliente = new Cliente(nome, cpf, email);
-        listaClientes.add(cliente);
-        validarEmailArroba(email);
-        verificarEmailClienteExistente(email);
-        verificarCpfClienteExistente(cpf);
-        System.out.println("\n\tCadastro cliente feito com sucesso!!!");
-        return cliente;
-    }
-
-   /* public static void verificarClienteCadastrado(String email, String cpf) throws Exception {
-        for (Cliente referenciaCliente : listaClientes) {
-            validarEmailArroba(email);
-            verificarEmailClienteExistente(referenciaCliente.getEmail(), email);
-            verificarCpfClienteExistente(referenciaCliente.getCpf(), cpf);
-        }
-    }*/
-
     public static void validarEmailArroba(String email) throws Exception {
         if (!email.contains("@")) {
             throw new Exception("O Email digitado não é válido. Tente novamente!");
@@ -54,27 +36,23 @@ public class ServiceCliente {
                 throw new Exception("Esse CPF não está cadastrado! Faça o cadastro primeiramente!");
         }
     }
-   /* public void validarEmailArroba(String email) throws Exception {
-        if (!email.contains("@")) {
-            throw new Exception("O Email digitado não é válido. Tente novamente!");
-        }
+
+    public static Cliente cadastrarCliente(String nome, String cpf, String email) throws Exception {
+        Cliente cliente = new Cliente(nome, cpf, email);
+        listaClientes.add(cliente);
+        validarEmailArroba(email);
+        verificarEmailClienteExistente(email);
+        verificarCpfClienteExistente(cpf);
+        System.out.println("\n\tCadastro cliente feito com sucesso!!!");
+        return cliente;
     }
 
-    public static void validarEmailRepetido(String email) throws Exception {
-        for (Cliente referenciaCliente : listaClientes) {
-            if (referenciaCliente.getEmail().equalsIgnoreCase(email)) {
-                throw new Exception("Esse e-mail já foi cadastrado! Tente novamente!");
-            } else {
-                throw new Exception("E-mail não encontrado! Cadastre primeiramente!");
-            }
+    public static void listarClientes() {
+
+        System.out.println("\n\t\t\t____Vendas realizadas____");
+        for (int i = 0; i < listaClientes.size(); i++) {
+            System.out.println(i);
+
         }
     }
-
-    public static void validarCpfRepetido(String cpf) throws Exception {
-        for (Cliente referenciaCliente : listaClientes) {
-            if (referenciaCliente.getCpf().equalsIgnoreCase(cpf))
-                throw new Exception("Esse CPF já foi cadastrado! Tente novamente!");
-        }
-    }*/
-
 }
