c18620228ddf98eecadd7566dbbedae85c9dea93
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 13:23:30 -0300

    Criar cadastrarVenda

--- src/ServiceVenda.java
+++ src/ServiceVenda.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceVenda {
+    //cadastrar as vendas
+    //listar as vendas
+    private static List<Venda> listaVendas = new ArrayList<>();
+
+    public static Venda cadastrarVenda(Cliente cliente, Vendedor vendedor, double valor, String data) {
+        Venda venda = new Venda(cliente, vendedor, valor, data);
+        listaVendas.add(venda);
+        return venda;
+
+    }
 }
